f7ebc3706a0824889926644abbfb08e29c8a4614
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-01-01 17:37:27 -0500

    grammar fixes

---
+++ out.py
@@ -0,0 +1,39 @@
+def junk ():
+    if found_q :
+        if inCompilingState_q :
+            if immediate_q :
+                
+                exec  ()
+            else: 
+                
+                compile_word  ()
+            
+        else: 
+            
+            exec  ()
+        
+    else: 
+        if inCompilingState_q :
+            if int_q :
+                
+                compile_int  ()
+            else: 
+                if float_q :
+                    
+                    compile_float  ()
+                else: 
+                    
+                    error  ()
+                
+            
+        else: 
+            if int_q :
+                
+                push_as_int  ()
+            else: 
+                if float_q :
+                    
+                    push_as_float  ()
+                else: 
+                    
+                    error  ()
--- temp.rewrite.mjs
+++ temp.rewrite.mjs
@@ -17,30 +17,80 @@ function getParameter (name) {
 
 let _rewrite = {
 
-main : function (cs,) {
-enter_rule ("main");
-    set_return (`${cs.rwr ().join ('')}`);
-return exit_rule ("main");
+Main : function (YesNo,) {
+enter_rule ("Main");
+    set_return (`def junk ():⤷\n${YesNo.rwr ()}`);
+return exit_rule ("Main");
 },
-char_commanlbrace : function (_,) {
-enter_rule ("char_commanlbrace");
-    set_return (`\n}`);
-return exit_rule ("char_commanlbrace");
+YesNo_meta : function (lb,sym,arg,y,n,rb,) {
+enter_rule ("YesNo_meta");
+    set_return (`if ${sym.rwr ()} ${arg.rwr ().join ('')}:⤷${y.rwr ()}⤶\n else: ⤷${n.rwr ()}⤶\n`);
+return exit_rule ("YesNo_meta");
 },
-char_commabrace : function (_,) {
-enter_rule ("char_commabrace");
-    set_return (`}`);
-return exit_rule ("char_commabrace");
+YesNo_expr : function (lb,sym,y,n,rb,) {
+enter_rule ("YesNo_expr");
+    set_return (`if ${sym.rwr ()}:⤷${y.rwr ()}⤶\n else:⤷${n.rwr ()}⤶\n`);
+return exit_rule ("YesNo_expr");
 },
-char_bracecommabracket : function (_,) {
-enter_rule ("char_bracecommabracket");
-    set_return (`}]`);
-return exit_rule ("char_bracecommabracket");
+YesBranch : function (_or,_yes,_,x,) {
+enter_rule ("YesBranch");
+    set_return (`\n${x.rwr ()}`);
+return exit_rule ("YesBranch");
 },
-char_c : function (c,) {
-enter_rule ("char_c");
-    set_return (`${c.rwr ()}`);
-return exit_rule ("char_c");
+NoBranch : function (_or,_no,_,x,) {
+enter_rule ("NoBranch");
+    set_return (`\n${x.rwr ()}`);
+return exit_rule ("NoBranch");
+},
+Process_withactual : function (_at,proc,actual,) {
+enter_rule ("Process_withactual");
+    set_return (`\n${proc.rwr ()} ${actual.rwr ()}`);
+return exit_rule ("Process_withactual");
+},
+Process_withoutactual : function (_at,proc,) {
+enter_rule ("Process_withoutactual");
+    set_return (`\n${proc.rwr ()} ()`);
+return exit_rule ("Process_withoutactual");
+},
+Actual : function (lp,actual,rp,) {
+enter_rule ("Actual");
+    set_return (`(${actual.rwr ()})`);
+return exit_rule ("Actual");
+},
+Arg : function (lb,name,rb,) {
+enter_rule ("Arg");
+    set_return (`(${name.rwr ()})`);
+return exit_rule ("Arg");
+},
+metaSymbol_percent : function (_pct,name,) {
+enter_rule ("metaSymbol_percent");
+    set_return (`pct_${name.rwr ()}`);
+return exit_rule ("metaSymbol_percent");
+},
+metaSymbol_question : function (name,_q,) {
+enter_rule ("metaSymbol_question");
+    set_return (`${name.rwr ()}_q`);
+return exit_rule ("metaSymbol_question");
+},
+symbol : function (name,) {
+enter_rule ("symbol");
+    set_return (`${name.rwr ()}`);
+return exit_rule ("symbol");
+},
+actual : function (name,) {
+enter_rule ("actual");
+    set_return (`${name.rwr ()}`);
+return exit_rule ("actual");
+},
+processName : function (name,) {
+enter_rule ("processName");
+    set_return (`${name.rwr ()}`);
+return exit_rule ("processName");
+},
+name : function (letter,cs,) {
+enter_rule ("name");
+    set_return (`${letter.rwr ()}${cs.rwr ().join ('')}`);
+return exit_rule ("name");
 },
 _terminal: function () { return this.sourceString; },
 _iter: function (...children) { return children.map(c => c.rwr ()); }
