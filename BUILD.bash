ec8acfd6c0bc6db572f7a03e30d300cb1c999a94
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-03 05:36:40 -0500

    begin RUN - BUILD.bash just calls RUN

--- BUILD.bash
+++ BUILD.bash
@@ -1,15 +1,2 @@
 #!/bin/bash
-rm -f out.*
-rm -f *.json
-set -e
-export SHELLOPTS
-export PBP=~/projects/pbp-dev
-export PBPHERE=$(pwd)
-export PYTHONPATH="${PBP}/kernel:${PYTHONPATH}"
-${PBP}/das2json dtree-transmogrifier.drawio
-python main.py 'example.drawio' main dtree-transmogrifier.drawio.json | ${PBP}/splitoutputs
-if [ -f out.✗ ]
-then
-    cat out.✗
-fi
-
+./RUN
