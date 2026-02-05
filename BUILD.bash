a1c6be7379b95903f444e8735a8ce5e3b6fc3b04
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-05 16:16:48 -0500

    remove debugs

--- BUILD.bash
+++ BUILD.bash
@@ -1,13 +1,7 @@
 #!/bin/bash
-echo >/tmp/pbplog.md
-echo >>/tmp/pbplog.md
-echo '--- in DTREE ---' >&2 >>/tmp/pbplog.md
-echo >&2 >>/tmp/pbplog.md
 export PBP=~/projects/pbp-dev
 export PBPWD=$(pwd)
 export PYTHONPATH="${PBP}/kernel:${PYTHONPATH}"
 export PBPCALLER=$PBPWD
+${PBP}/resetlog
 ./RUN "example" $PBWD $PBPCALLER
-echo >&2 >>/tmp/pbplog.md
-echo '--- done DTREE ---' >&2 >>/tmp/pbplog.md
-echo >&2 >>/tmp/pbplog.md
