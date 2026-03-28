b705f4f1f62ab65527c0c7593b5f03bc7d2acfba
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-03-28 08:36:54 -0400

    switch to kernel with tempfile for shellout ; rm pbp-dev dtree and sm

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import sys
 import kernel0d as zd
 
+import fcntl, os
+
+## for logging during debug
+# log_path = os.path.join(os.environ.get('PBPWD', '.'), 'pbplog.txt')
+
+# def pbplog(msg):
+#     with open(log_path, 'a') as f:
+#         f.write(msg + '\n')
+
 try:
 
     [palette, env] = zd.initialize_from_files (sys.argv[3:])
--- support.mjs
+++ support.mjs
@@ -1,3 +1,19 @@
+/////// <pbp logging>
+//const fs = require('fs');
+//const path = require('path');
+import * as path from 'path';
+
+function pbplog(message) {
+  const baseDir = process.env.PBPCALLER;
+  if (!baseDir) throw new Error('PBPCALLER env var not set');
+  
+  const logFile = path.join(baseDir, 'pbplog.txt');
+  fs.appendFileSync(logFile, `${message}\n`, 'utf8');
+}
+
+//fs.writeFileSync('/tmp/@pbplog.txt', new Date().toISOString() + '\n');
+/////// </pbp logging>
+
 let counter = 2;
 let dict = {};
 
